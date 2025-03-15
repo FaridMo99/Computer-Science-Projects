c0bac16be522a64c970cc710e788331c2d2a9e58
FaridMo99 <farid-ahmad@hotmail.de>
2025-03-15 19:33:56 +0100

    finished recursion project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Computer-Science-Projects
+
+## Recursion
+
+## Linked-Lists
+
+## Knights-Travails
+
+## HashMap
+
+## Binary-Search-Trees
---
+++ Recursion/mergeSort.js
@@ -0,0 +1,27 @@
+export default function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+    const middle = Math.floor(arr.length / 2);
+
+    const left = mergeSort(arr.slice(0, middle));
+    const right = mergeSort(arr.slice(middle));
+
+    return merge(left, right);
+}
+
+function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result.concat(left.slice(leftIndex), right.slice(rightIndex));
+}
