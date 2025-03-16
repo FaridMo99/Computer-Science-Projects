8454ecbf6b20df8b968a8d9e8be5cadf9dccf6c6
FaridMo99 <farid-ahmad@hotmail.de>
2025-03-16 20:20:09 +0100

    finished linked list

---
+++ Linked-Lists/module.js
@@ -0,0 +1,135 @@
+export default class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (this.head === null) {
+        this.head = newNode;
+        return;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode !== null && currentNode.nextNode !== null) {
+      currentNode = currentNode.nextNode;
+    }
+
+    currentNode.nextNode = newNode
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.nextNode = this.head;
+    this.head = newNode;
+  }
+
+  getSize() {
+    let counter = 0;
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      counter++;
+      currentNode = currentNode.nextNode;
+    }
+
+    return counter;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  getTail() {
+    let currentNode = this.head;
+
+    while (currentNode !== null && currentNode.nextNode !== null) {
+      currentNode = currentNode.nextNode;
+    }
+
+    return currentNode;
+  }
+
+  at(index) {
+    let currentNode = this.head;
+
+    for (let i = 0; i < index; i++) {
+      if (currentNode === null) {
+        return null;
+      }
+      currentNode = currentNode.nextNode;
+    }
+
+    return currentNode;
+  }
+
+  pop() {
+    if (this.head === null) {
+      return;
+    }
+
+    if (this.head.nextNode === null) {
+      this.head = null;
+      return;
+    }
+
+    let currentNode = this.head;
+
+    while (
+      currentNode.nextNode !== null &&
+      currentNode.nextNode.nextNode !== null
+    ) {
+      currentNode = currentNode.nextNode;
+    }
+
+    currentNode.nextNode = null;
+  }
+
+  contains(value) {
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      if (currentNode.value == value) {
+        return true;
+      }
+      currentNode = currentNode.nextNode;
+    }
+  }
+
+  find(value) {
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      if (currentNode.value === value) {
+        return currentNode;
+      }
+      currentNode = currentNode.nextNode;
+    }
+    return null;
+  }
+
+  toString() {
+    let currentNode = this.head;
+    let arrString = [];
+
+    while (currentNode !== null) {
+        arrString.push(` ${currentNode.value.toString()} -> `)
+      currentNode = currentNode.nextNode;
+    }
+
+    if(currentNode === null){
+        arrString.push("null")
+    }
+
+    return arrString.join("")
+  }
+}
+
+export class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
--- Recursion/main.js
+++ Recursion/main.js
@@ -1,36 +1,29 @@
-import mergeSort from "./mergeSort.js"
+import mergeSort from "./mergeSort.js";
 
+function fibs(n) {
+  let arr = [0, 1];
 
-function fibs(n){
-    let arr = [0,1]
+  if (n <= 0) return 0;
+  if (n === 1) return 1;
 
-    if (n <= 0) return 0;
-    if (n === 1) return 1;
-
-    if(n >= 2){
-
-    for(let i = 2; i < n; i++){
-        arr.push(arr[i-1] + arr[i-2])
-
+  if (n >= 2) {
+    for (let i = 2; i < n; i++) {
+      arr.push(arr[i - 1] + arr[i - 2]);
     }
-    }
-    return arr
+  }
+  return arr;
 }
 
+function fibsRec(n, arr = [0, 1]) {
+  console.log("This was printed recursively " + n + " times");
+  if (n <= 2) return arr;
 
-function fibsRec(n, arr = [0, 1]){
-
-    console.log("This was printed recursively " + n + " times")
-    if (n <= 2) return arr
-
-    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
-    return fibsRec(n - 1, arr)
+  arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+  return fibsRec(n - 1, arr);
 }
 
-console.log(fibs(8))
+console.log(fibs(8));
 
-console.log(fibsRec(8))
+console.log(fibsRec(8));
 
-console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]))
-
-
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
